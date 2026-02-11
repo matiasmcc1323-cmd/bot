bb862fe040bc5c1f9b168734c670e9fd7d752a52
matiasmcc1323-cmd <matiasmcc1323@gmail.com>
2026-02-11 18:04:50 -0500

    botest2.py

--- botest.py
+++ botest.py
@@ -1,5 +1,7 @@
 import discord
-
+from botlogic import gen_pass
+from botlogic import flip_a_coin
+from botlogic import random_number
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -17,6 +19,15 @@ async def on_message(message):
         await message.channel.send("Hi!")
     elif message.content.startswith('$bye'):
         await message.channel.send("\U0001f642")
+    elif message.content.startswith('$ranpass'):
+        password = gen_pass(10)
+        await message.channel.send(password)
+    elif message.content.startswith('$flipacoin'):
+        coin = flip_a_coin()
+        await message.channel.send(coin)
+    elif message.content.startswith('$rannum'):
+        number = random_number(100)
+        await message.channel.send(str(number))
     else:
         await message.channel.send(message.content)
 
