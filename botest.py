bb2436e7eeaf0e502459ae25d3fad4a60d0faf0b
matiasmcc1323-cmd <matiasmcc1323@gmail.com>
2026-02-11 18:02:38 -0500

    Create botest.py

---
+++ botest.py
@@ -0,0 +1,23 @@
+import discord
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+client.run("token")
